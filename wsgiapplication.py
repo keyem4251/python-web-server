f41bb8f216ebfb3d2040bb9930d2a2d5b62c8927
Emura Keisuke <emura.keisuke@panair.jp>
2020-10-30 17:02:22 +0900

    get parameterに対応

--- wsgiapplication.py
+++ wsgiapplication.py
@@ -56,6 +56,14 @@ class WSGIApplication:
         response_headers = self.create_response_headers(ext)
 
         try:
+            if self.env["REQUEST_METHOD"] == "GET":
+                query_string = self.env["QUERY_STRING"]
+                if query_string:
+                    query = dict()
+                    for pair in query_string.split("&"):
+                        key, value = pair.split("=")
+                        query[key] = value
+                    
             content = self.get_file_content(static_dir+abspath)
             content = self.fill_parameter(content)
             return "200 OK", [content], response_headers
