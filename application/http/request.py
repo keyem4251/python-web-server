830fffbd5279667fc22856fad74acbdc7a02911b
Emura Keisuke <emura.keisuke@panair.jp>
2020-11-05 17:21:50 +0900

    viewを動くように

--- application/http/request.py
+++ application/http/request.py
@@ -1,4 +1,3 @@
-
 class Request:
     env: dict
     method: str
@@ -14,7 +13,7 @@ class Request:
         self.headers = dict()
         self.headers["CONTENT_TYPE"] = self.env["CONTENT_TYPE"]
         self.headers["CONTENT_LENGTH"] = self.env["CONTENT_LENGTH"]
-        for k, v in self.env:
+        for k, v in self.env.items():
             if k.startswith("HTTP_"):
                 self.headers[k] = v
         self.GET = dict()
@@ -31,21 +30,3 @@ class Request:
     @property
     def body(self) -> bytes:
         return self.env["wsgi.input"].read()
-
-    @property
-    def query_dict(self):
-        query_string = ""
-        if self.method == "GET":
-            query_string = self.env["QUERY_STRING"]
-        elif self.method == "POST":
-            if self.headers["CONTENT_TYPE"] == "application/x-www-form-urlencoded":
-                query_string = self.parse_parameter(body.decode())
-
-        query_dict = dict()
-        if not query_string:
-            return query
-
-        for pair in query_string.split("&"):
-            key, value = pair.split("=")
-            query_dict[key] = value
-        return query_dict
--- application/views/parameters.py
+++ application/views/parameters.py
@@ -1,8 +1,54 @@
+import os
+import datetime
+from typing import List
+
 from application.http.request import Request
 from application.http.response import Response
 
 
 class ParametersView:
+    content_type = {
+        "html": "text/html",
+        "htm": "text/html",
+        "txt": "text/plain",
+        "css": "text/css",
+        "png": "image/png",
+        "jpg": "image/jpeg",
+        "jpeg": "image/jpeg",
+        "gif": "image/gif",
+    }
+
+    def get_content_type(self, ext: str):
+        return self.content_type.get(ext, "application/octet-stream")
+
+    @staticmethod
+    def get_date_string_utc():
+        return datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+    @staticmethod
+    def get_file_content(path: str):
+        with open(path, "rb") as f:
+            return f.read()
+
+    def create_response_headers(self, ext: str) -> List:
+        response_headers = [
+            ('Content-type', self.get_content_type(ext)),
+            ("Date", self.get_date_string_utc()),
+            ("Server", "Modoki/0.3"),
+            ("Connection", "close")
+        ]
+        return response_headers
+
+    @staticmethod
+    def get_ext(abspath: str) -> str:
+        if abspath.endswith("/"):
+            ext = "html"
+        elif not abspath.endswith("/") and "." not in abspath:
+            ext = "html"
+        else:
+            ext = abspath.split(".")[1]
+        return ext
+
     def get_response(self, request: Request) -> Response:
         abspath = request.path
         root = os.getcwd()
