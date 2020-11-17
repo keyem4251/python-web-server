ff6a286d80ce9b917f593e8dbc9a313a69346c88
Emura Keisuke <emura.keisuke@panair.jp>
2020-11-17 16:07:06 +0900

    content fileの取得をstaticのviewクラスに移動

--- application/views/base.py
+++ application/views/base.py
@@ -3,26 +3,13 @@ from application.http.response import Response, HTTP_STATUS
 
 
 class BaseView:
-    content_type: dict
-
-    def __init__(self):
-        self.content_type = {
-            "html": "text/html",
-            "htm": "text/html",
-            "txt": "text/plain",
-            "css": "text/css",
-            "png": "image/png",
-            "jpg": "image/jpeg",
-            "jpeg": "image/jpeg",
-            "gif": "image/gif",
-        }
-
     def get_response(self, request: Request) -> Response:
         if request.method == "GET":
             return self.get(request)
 
         elif request.method == "POST":
             return self.post(request)
+
         else:
             return Response(status=HTTP_STATUS.METHOD_NOT_ALLOWED)
 
@@ -31,17 +18,3 @@ class BaseView:
 
     def post(self, request: Request) -> Response:
         return Response(status=HTTP_STATUS.METHOD_NOT_ALLOWED)
-
-    def get_content_type(self, request: Request):
-        ext = self.get_ext(request.path)
-        return self.content_type.get(ext, "application/octet-stream")
-
-    @staticmethod
-    def get_ext(abspath: str) -> str:
-        if abspath.endswith("/"):
-            ext = "html"
-        elif not abspath.endswith("/") and "." not in abspath:
-            ext = "html"
-        else:
-            ext = abspath.split(".")[1]
-        return ext
--- application/views/static.py
+++ application/views/static.py
@@ -6,6 +6,33 @@ from application.utils import get_file_content
 
 
 class StaticView(BaseView):
+    content_type: dict
+
+    def __init__(self):
+        self.content_type = {
+            "html": "text/html",
+            "htm": "text/html",
+            "txt": "text/plain",
+            "css": "text/css",
+            "png": "image/png",
+            "jpg": "image/jpeg",
+            "jpeg": "image/jpeg",
+            "gif": "image/gif",
+        }
+
+    def get_content_type(self, request: Request):
+        ext = self.get_ext(request.path)
+        return self.content_type.get(ext, "application/octet-stream")
+
+    @staticmethod
+    def get_ext(abspath: str) -> str:
+        if abspath.endswith("/"):
+            ext = "html"
+        elif not abspath.endswith("/") and "." not in abspath:
+            ext = "html"
+        else:
+            ext = abspath.split(".")[1]
+        return ext
 
     def get(self, request: Request) -> Response:
         filename = request.path.replace("/static/", "")
