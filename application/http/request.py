3a956e868878bdc6025dec8585ec44c37b360ea8
Emura Keisuke <emura.keisuke@panair.jp>
2020-11-19 17:41:36 +0900

    requestをdataclassに

--- application/http/request.py
+++ application/http/request.py
@@ -1,23 +1,25 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
 class Request:
     env: dict
-    method: str
-    path: str
-    headers: dict
-    cookies: dict
-    GET: dict
-    POST: dict
+    method: str = ""
+    path: str = ""
+    headers: dict = None
+    cookies: dict = None
+    GET: Optional[dict] = None
+    POST: Optional[dict] = None
 
-    def __init__(self, env: dict):
-        self.env = env
+    def __post_init__(self):
         self.method = self.env["REQUEST_METHOD"]
         self.path = self.env["PATH_INFO"]
-        self.headers = dict()
+
+        if self.headers is None:
+            self.headers = {}
         self.headers["CONTENT_TYPE"] = self.env["CONTENT_TYPE"]
         self.headers["CONTENT_LENGTH"] = self.env["CONTENT_LENGTH"]
-
-        for k, v in self.env.items():
-            if k.startswith("HTTP_"):
-                self.headers[k] = v
 
         if "HTTP_COOKIE" in self.headers:
             cookies_list = self.headers["HTTP_COOKIE"].split("; ")
@@ -26,13 +28,18 @@ class Request:
                 key, value = cookie.split("=")
                 self.cookies[key] = value
 
-        self.GET = dict()
-        self.POST = dict()
+        if self.GET is None:
+            self.GET = {}
+
         if self.method == "GET" and self.env["QUERY_STRING"]:
             for pair in self.env["QUERY_STRING"].split("&"):
                 key, value = pair.split("=")
                 self.GET[key] = value
-        elif self.method == "POST" and self.headers["CONTENT_TYPE"] == "application/x-www-form-urlencoded":
+
+        if self.POST is None:
+            self.POST = {}
+
+        if self.method == "POST" and self.headers["CONTENT_TYPE"] == "application/x-www-form-urlencoded":
             for pair in self.body.decode().split("&"):
                 key, value = pair.split("=")
                 self.POST[key] = value
